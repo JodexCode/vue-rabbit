20dd13480b1c796c5a8d5c855228b0760b7479bb
itJie <3362670372@qq.com>
2025-04-18 23:49:25 +0800

    设置一级路由和二级路由Home,Category

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,38 @@
-import { createRouter, createWebHistory } from "vue-router";
+// createRouter: 创建路由实例
+// createWebHistory: 创建history模式的路由
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Layout from '@/views/Layout/index.vue'
+import Login from '@/views/Login/index.vue'
+import Home from '@/views/Layout/Home/index.vue'
+import Category from '@/views/Layout/Category/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-});
+  routes: [
+    {
+      path: '/',
+      name: 'Layout',
+      component: Layout,
+      children: [
+        {
+          path: '',
+          name: 'Home',
+          component: Home
+        },
+        {
+          path: 'category',
+          name: 'Category',
+          component: Category
+        }
+      ]
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    }
+  ]
+})
 
-export default router;
+export default router
--- src/views/Layout/Category/index.vue
+++ src/views/Layout/Category/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-view />
+  <div>Category</div>
 </template>
 
 <script lang="ts" setup>
