88e788a303764d33a6dd4876f6402ff18f142e7f
itJie <3362670372@qq.com>
2025-04-18 19:15:25 +0800

    elementPlus主题色定制

--- vite.config.js
+++ vite.config.js
@@ -18,12 +18,24 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+        // 配置elementPlus采用sass样式配色系统
+        ElementPlusResolver({ importStyle: "sass" }),
+      ],
     }),
   ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        //自动导入定制化样式文件进行样式覆盖
+        //注意：如果使用了elementPlus，需要在main.js中引入elementPlus的定制化样式文件，否则会导致elementPlus的样式覆盖失效
+        additionalData: `@use "@/styles/element/index.scss" as *;`,
+      },
     },
   },
 });
