45715c3a4fca2aef776a1ddfddd09e0e2bb0fb68
itJie <3362670372@qq.com>
2025-04-19 23:26:14 +0800

    home-整体结构拆分以及分类的数据渲染

---
+++ src/views/Layout/routes/Home/components/HomeProduct.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>HomeProduct</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped></style>
--- src/views/Layout/routes/Home/index.vue
+++ src/views/Layout/routes/Home/index.vue
@@ -1,9 +1,18 @@
-<template>
-  <div>Home</div>
-</template>
-
-<script lang="ts" setup>
-
+<script setup>
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import HomeProduct from './components/HomeProduct.vue'
 </script>
 
-<style scoped></style>
+<template>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+
+  <HomeNew />
+  <HomeHot />
+  <HomeProduct />
+</template>
