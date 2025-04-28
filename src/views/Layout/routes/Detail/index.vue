f3e9e533b884cb130cb8661994f4bec515ca2f07
itJie <3362670372@qq.com>
2025-04-28 13:56:44 +0800

    详情热榜-基础组件封装和基础数据获取渲染

--- src/views/Layout/routes/Detail/index.vue
+++ src/views/Layout/routes/Detail/index.vue
@@ -1,4 +1,5 @@
 <script setup>
+import DetailHot from './components/DetailHot.vue'
 import { getDetail } from '@/apis/detail.js'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
@@ -120,7 +121,10 @@ onMounted(() => {
             </div>
 
             <!-- 24热榜+专题推荐 -->
-            <div class="goods-aside"></div>
+            <div class="goods-aside">
+              <DetailHot />
+              <DetailHot />
+            </div>
           </div>
         </div>
       </div>
