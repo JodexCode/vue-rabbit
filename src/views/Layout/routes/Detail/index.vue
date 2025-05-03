7aae3468861334cd4ba73ded9e8b4c1d546986a2
itJie <3362670372@qq.com>
2025-05-03 14:14:31 +0800

    图片预览组件-小图切换大图显示

--- src/views/Layout/routes/Detail/index.vue
+++ src/views/Layout/routes/Detail/index.vue
@@ -1,5 +1,6 @@
 <script setup>
 import DetailHot from './components/DetailHot.vue'
+import imageView from '@/components/imageView/index.vue'
 import { getDetail } from '@/apis/detail.js'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
@@ -36,7 +37,7 @@ onMounted(() => {
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-
+              <image-view />
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
